5fcc520038c079867be45a11b817fc1393182513
Nicolas Landucci <74741743+nicolandu@users.noreply.github.com>
2024-07-21 17:06:50 -0400

    Add signature, add skeleton toggle

--- fractal_canopy.blot.js
+++ fractal_canopy.blot.js
@@ -3,21 +3,31 @@
 const width = 125;
 const height = 125;
 
+// interesting effect (toggle)
+const showSkeleton = true;
 
-
+// determines next branch length
 function lenMapping(x) {
   return x*.75;
 }
 
+// length of trunk
+const initialLen = 28;
+
+// determines branch length threshold
 const minLen = 1;
 const angle = 20;
 
+// base value
 const randomFactor = 0.05;
+// how much randomFactor increases at each sample on a branch
 const randomFactorProgression = 0.05;
 
+// we only use the Turtle to measure lines, not to draw them, unless showSkeleton is active
+// in any case, pen down is OK
 const t = new bt.Turtle()
   .left(90)
-  .down();
+.down();
 
 setDocDimensions(width, height);
 
@@ -28,7 +38,8 @@ const allLines = [];
 function rec(x) {
   if (x<minLen) return;
   const line = [];
-  t.down();
+  const initialPos = t.pos;
+  const initialAngle = t.angle;
   line.push(t.pos);
   for (let i = 0; i < 3; i++) {
     t.forward(x/4);
@@ -50,23 +61,63 @@ function rec(x) {
   rec(lenMapping(x));
   t.right(2*angle);
   rec(lenMapping(x));
-  t.left(angle);
-  t.forward(-x);
+  t.jump(initialPos);
+  t.setAngle(initialAngle);
 } 
 
-rec(28);
+rec(initialLen);
 console.log(allLines);
 for (let line in allLines) {
   const tmp = [bt.catmullRom(allLines[line])];
   bt.join(finalLines, tmp);
 }
 
+if (showSkeleton) bt.join(finalLines, t.path);
 
 bt.translate(
   finalLines,
   [width / 2, height / 2],
   bt.bounds(finalLines).cc
 );
+
+const signaturePoints = [
+  [-2.07, 1.26],
+  [0, 2],
+  [1.33, 3.4],
+  [1.22, 4.65],
+  [0.18, 5.5],
+  [-0.93, 4.29],
+  [-1.64, -1.8],
+  [-0.68, 0.79],
+  [0.54, 1.06],
+  [1.67, -2],
+  [3.49, -1.29],
+  [4.84, 2.55],
+  [3.41, 6.11],
+  [-1.22, 7.39],
+  [-4.14, 4.86],
+  [-4.5, 1.76],
+  [-3.44, -1.43],
+];
+
+for (let i = 0; i < signaturePoints.length; i++) {
+  signaturePoints[i][0] += bt.randInRange(-0.2,0.2);
+  signaturePoints[i][1] += bt.randInRange(-0.2,0.2);
+}
+
+const signature = [bt.catmullRom(signaturePoints)];
+
+bt.rotate(signature, bt.randInRange(-2,2), bt.bounds(signature).cc);
+bt.translate(
+  signature,
+  [
+    width/2+bt.randInRange(-0.5,0.5),
+    height/2+bt.randInRange(-0.5,0.5)
+  ],
+  bt.bounds(signature).cc
+);
+
+bt.join(finalLines, signature);
 
 // draw it
 drawLines(finalLines);
