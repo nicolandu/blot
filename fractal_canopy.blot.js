2520f98eeb2888eb8a112fe4a54b5155c9b621ca
Nicolas Landucci <74741743+nicolandu@users.noreply.github.com>
2024-07-21 14:56:50 -0400

    Started randomizing straight lines

--- fractal_canopy.blot.js
+++ fractal_canopy.blot.js
@@ -6,56 +6,60 @@ const height = 125;
 
 
 function lenMapping(x) {
-  return x-1;
+  return x*.75;
 }
+
+const minLen = 1;
+const angle = 20;
+
+const randomFactor = 0.15;
 
 const t = new bt.Turtle()
   .left(90)
   .down();
 
+setDocDimensions(width, height);
+
 // store final lines here
 const finalLines = [];
 
 function rec(x) {
-  if (x<1) return;
+  if (x<minLen) return;
   const line = [];
   t.down();
   line.push(t.pos);
-  for (let i = 0; i < 4; i++) {
-    t.forward(x);
+  for (let i = 0; i < 3; i++) {
+    t.forward(x/4);
+    t.left(90);
+    const step = bt.randInRange(-randomFactor*x, randomFactor*x);
+    t.forward(step);
     line.push(t.pos);
+    t.forward(-step);
+    t.right(90);
   }
+  t.forward(x/4);
+  line.push(t.pos);
+  
   //finalLines.push(line);
-  t.left(45);
+  t.left(angle);
   rec(lenMapping(x));
-  t.right(90);
+  t.right(2*angle);
   rec(lenMapping(x));
-  t.left(45);
-  t.forward(-4*x);
+  t.left(angle);
+  t.forward(-x);
 }
 
   
   
 
-rec(15);
+rec(28);
 
-/*const lines = [[
-    [0, 1],
-    [0, -1]
-  ],
-  [
-    [1, 0],
-    [0, 0]
-  ]];
-// centre mark, 2x2 mm*/
 bt.join(finalLines, t.path);
-
-/*bt.offset(lines, 2, { arcTolerance: 0.001 });
-bt.join(finalLines, lines);*/
 
 bt.translate(
   finalLines,
   [width / 2, height / 2],
+  bt.bounds(finalLines).cc
 );
 
 // draw it
