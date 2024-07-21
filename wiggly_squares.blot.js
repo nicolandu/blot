a470cdbb88d288aa8d67df4d706f94eb3780e1ee
Nicolas Landucci <74741743+nicolandu@users.noreply.github.com>
2024-07-21 10:48:35 -0400

    Add signature

--- wiggly_squares.blot.js
+++ wiggly_squares.blot.js
@@ -1,9 +1,13 @@
 // Wiggly Squares by Nicolas Landucci is GPLv3-licensed.
+// mm
 const width = 125;
 const height = 125;
 
 // multiple of 2
 const gridDim = 8;
+
+//mm
+const gridInterval = 12;
 
 function rect(w, h) {
   return [
@@ -28,7 +32,7 @@ for (let i = -gridDim + 1; i < gridDim; i += 2) {
   const colPos = [];
   for (let j = -gridDim + 1; j < gridDim; j += 2) {
     const square = rect(10, 10);
-    const shapeCenter = [i * 6, j * 6];
+    const shapeCenter = [i * gridInterval / 2, j * gridInterval / 2];
     bt.translate(
       square,
       shapeCenter,
@@ -88,6 +92,37 @@ for (let ring = 0; ring < (gridDim / 2); ring++) {
 const curve = [bt.catmullRom(allPosSpiral)];
 bt.offset(curve, 1, { arcTolerance: 0.001 });
 bt.join(finalLines, curve);
+
+const signaturePoints = [
+  [-2.07, 1.26],
+  [0, 2],
+  [1.33, 3.4],
+  [1.22, 4.65],
+  [0.18, 5.5],
+  [-0.93, 4.29],
+  [-1.64, -1.8],
+  [-0.65, 0.79],
+  [0.54, 1.06],
+  [1.67, -2],
+  [3.49, -1.29],
+  [4.84, 2.55],
+  [3.41, 6.11],
+  [-1.22, 7.39],
+  [-4.14, 4.86],
+  [-4.5, 1.76],
+  [-3.44, -1.43],
+];
+
+const signature = [bt.catmullRom(signaturePoints)];
+
+bt.translate(
+  signature,
+  [
+    (gridDim + 1) / 2 * gridInterval-1,
+    -(gridDim + 1) / 2 * gridInterval-1
+  ]);
+
+bt.join(finalLines, signature);
 
 bt.translate(
   finalLines,
